929eb4190bba12402cd7a7f90f39db9c19bd9952
Cedric Ziel <cedric@cedric-ziel.com>
2016-04-09 22:24:53 +0200

    Return print_r instead of var_dump'ing

--- web/index.php
+++ web/index.php
@@ -183,7 +183,7 @@ $app->get(
     function () {
         $serverVars = $_SERVER;
         unset($serverVars['TELEGRAM_TOKEN']);
-        $content = var_dump($serverVars, true);
+        $content = print_r($serverVars, true);
 
         return new Response('<html><body><pre>'.$content.'</pre></body></html>');
     }
