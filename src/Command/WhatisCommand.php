f5b613a96171755bafafb1b97f7e59baf82f58fa
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-04-16 21:40:42 +0200

    Parse exception with HTML

--- src/Command/WhatisCommand.php
+++ src/Command/WhatisCommand.php
@@ -69,7 +69,10 @@ class WhatisCommand extends Command
                 }
             }
         } catch (ClientException $e) {
-            $this->replyWithMessage(['text' => '<a href="https://github.com/3lustige2/schettbott/wiki/1460833560">Uups</a>.']);
+            $this->replyWithMessage([
+                'text' => '<a href="https://github.com/3lustige2/schettbott/wiki/1460833560">Uups</a>.',
+                'parse_mode' => 'HTML'
+            ]);
         }
     }
 }
